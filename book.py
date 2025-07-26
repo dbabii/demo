aa44a747a780d219910bb7e73039b485fa503a3c
Dima <dm.babii@gmail.com>
2025-07-26 13:11:49 +0100

    creating a Class methods, creating an empy library, adding view_books method

---
+++ book.py
@@ -0,0 +1,28 @@
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def describe(self):
+        print(f"The title of the books is {self.title}")
+        print(f"The author is {self.author}")
+        print(f"The book was published in {self.year}")
+
+
+library = []
+
+def add_book():
+    title = input("Please enter a title: ")
+    author = input("Please enter an author: ")
+    year = int(input("Please enter a year: "))
+    
+    book = Book(title, author, year)
+    return book
+
+def view_books():
+    if len(library) == 0:
+        print("Library is empty")
+    else:
+        for i in library:
+            i.describe()
