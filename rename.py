bba095ad13c7ed0f365edd451e43277aab93c4dd
Dima <dm.babii@gmail.com>
2025-07-26 14:43:31 +0100

    save extension, provide a files' list before and after'

--- rename.py
+++ rename.py
@@ -4,11 +4,21 @@ import os
 folder_path = "/home/grom/git/demo/dir_txt"
 file_list = os.listdir(folder_path)
 
-for i in file_list:
-    print(i)
+def print_files():
+    file_list = os.listdir(folder_path)
+    for i in file_list:
+        print(i)
+
+print("Before changing:")
+print_files()
 
 for i, file in enumerate(file_list, start=1):
     old_path = os.path.join(folder_path, file)
-    name = f"file_{i}.txt"
+    # save original extension
+    ext = os.path.splitext(file)[1]
+    name = f"file_{i}{ext}"
     new_path = os.path.join(folder_path, name)
     os.rename(old_path, new_path)
+
+print("After changing:")
+print_files()
